8eace17fe73a82331761e6d3bd6b2f7f9726c249
Thais Pereira <59610437+thataaz@users.noreply.github.com>
2022-11-02 10:10:40 -0300

    feat: add date validation

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -5,15 +5,26 @@ import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class ReajusteService {
 
     public void reajustarSalario(Funcionario funcionario, BigDecimal aumento){
         BigDecimal salarioAtual = funcionario.getSalario();
         BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario");
         }
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser no minimo de 6 meses");
+        }
+
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
     }
