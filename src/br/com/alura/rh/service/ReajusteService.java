97e38a07aedc65b5e03cdc10d1e6e03659334335
Thais Pereira <59610437+thataaz@users.noreply.github.com>
2022-11-02 10:46:25 -0300

    refactor: apply open closed principle
    - abstrair a regra de validar para classes que implementam a interface.
    - Com isso conseguimos deixar as classes serem abertas para extensão, mas fechadas para modificação
    - a classe ReajusteService agora consegue ter mais validacoes, que foram abstraidas para outras classes

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -1,30 +1,23 @@
 package br.com.alura.rh.service;
 
-import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.List;
+
 
 public class ReajusteService {
 
+    private final List<ValidacaoReajuste> validacoes;
+
+    public ReajusteService(List<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
     public void reajustarSalario(Funcionario funcionario, BigDecimal aumento){
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
+
         BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario");
-        }
-
-        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
-        LocalDate dataAtual = LocalDate.now();
-        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
-        if (mesesDesdeUltimoReajuste < 6) {
-            throw new ValidacaoException("Intervalo entre reajustes deve ser no minimo de 6 meses");
-        }
-
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
     }
